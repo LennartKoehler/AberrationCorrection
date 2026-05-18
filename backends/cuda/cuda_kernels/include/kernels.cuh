a0024fd18c20da0167e370c98452089e114aad6f
Lennart Köhler HKI <lennart.koehler@leibniz-hki.de>
2026-05-18 10:51:16 +0200

    feat: cudaaberrationbackend

--- backends/cuda/cuda_kernels/include/kernels.cuh
+++ backends/cuda/cuda_kernels/include/kernels.cuh
@@ -10,9 +10,7 @@ typedef float real_t;
 
 typedef real_t complex_t[2];
 
-struct ZernikeCoefficients {
-    real_t c[4]; // Z0: Piston, Z1: Tilt X, Z2: Tilt Y, Z3: Defocus
-};
+using ZernikeCoefficients = real_t [4]; // Z0: Piston, Z1: Tilt X, Z2: Tilt Y, Z3: Defocus
 
 __global__ void applyPhaseCorrectionGlobal(int Nx, int Ny, int Nz, complex_t* data, const real_t* phaseX, const real_t* phaseY, const real_t* phaseZ);
 
--- backends/cuda/include/cuda/CUDAAberrationBackend.h
+++ backends/cuda/include/cuda/CUDAAberrationBackend.h
@@ -4,6 +4,8 @@
 #include "cuda_backend/CUDABackendManager.h"
 #include "aberration_backend/IAberrationBackend.h"
 
+
+
 class CUDAAberrationBackend : public CUDAComputeBackend, public IAberrationBackend{
 public:
     using CUDAComputeBackend::CUDAComputeBackend;
@@ -13,3 +15,14 @@ public:
     void computeAberration(const ComplexData& data) const override;
 };
 
+
+class CUDAAberrationBackendManager : public CUDABackendManager{
+
+public:
+    IComputeBackend& getComputeBackend(const BackendConfig& config) override{
+        auto deconv = std::make_unique<CUDAAberrationBackend>(configToConfig(config));
+        std::unique_lock<std::mutex> lock(mutex_);
+        computeBackends.push_back(std::move(deconv));
+        return *computeBackends.back();
+    }
+};
